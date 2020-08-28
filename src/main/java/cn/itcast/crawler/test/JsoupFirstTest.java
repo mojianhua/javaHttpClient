36fa97ff6048802d08cab27ead7c8f6ca93810f1
饭桶吉 <1657210793@qq.com>
2020-08-28 19:24:23 +0800

    dom test

--- src/main/java/cn/itcast/crawler/test/JsoupFirstTest.java
+++ src/main/java/cn/itcast/crawler/test/JsoupFirstTest.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Set;
 
 public class JsoupFirstTest {
 
@@ -31,7 +32,22 @@ public class JsoupFirstTest {
 
         //根据属性获取元素
         element = doc.getElementsByAttribute("att").first();
-        System.out.println("获取到的元素内容是：" + element.text());
+        //System.out.println("获取到的元素内容是：" + element.text());
+
+        //根据id获取元素
+        String str = "";
+        element = doc.getElementById("test1");
+        str = element.id();
+        //System.out.println("获取到的数据是：" + str);
+        //从元素中获取className
+        Set<String> classSet = element.classNames();
+        for (String s: classSet) {
+            System.out.println(s);
+        }
+
+        //从元素中获取属性的attr
+        str = element.attr("class");
+        System.out.println("获取到的数据是：" + str);
         //System.out.println(content);
     }
 }
